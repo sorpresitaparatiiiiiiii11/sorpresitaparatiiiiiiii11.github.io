d0fc828a04cdcbb79f966481d6cc3a5b249ea427
sorpresitaparatiiiiiiii11 <lgonzalesdlc@outlook.com>
2025-02-04 13:10:50 -0500

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('static-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/imagenes/conejito 1.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
